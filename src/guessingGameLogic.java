98ddf6f6334af18686ceb83597315227eb415f46
SuperSalcedo22 <janfeilsalcedo@gmail.com>
2024-01-25 17:06:08 +0000

    Created start panel GUI with no functionality to build on

--- src/guessingGameLogic.java
+++ src/guessingGameLogic.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-public class guessingGame {
+public class guessingGameLogic {
 
     public static void guessingNumberGame(Scanner scanner, int gamesWon) {
 
